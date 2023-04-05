f7c25b6528f53956e90a036c9488dab0367cdb77
ljka <ljka@viessmann.com>
2023-04-05 17:08:32 +0200

    instructions made during meeting 1.2

--- app/src/main/java/com/lyjak/anna/mymentoring/MainActivity.kt
+++ app/src/main/java/com/lyjak/anna/mymentoring/MainActivity.kt
@@ -3,13 +3,16 @@ package com.lyjak.anna.mymentoring
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.lyjak.anna.mymentoring.ui.theme.MyMentoringTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,15 +20,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyMentoringTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Screen()
             }
         }
+    }
+}
+
+@Composable
+fun Screen() {
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Greeting("Android")
+        Goodby("Programmer")
     }
 }
 
@@ -34,10 +43,20 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun Goodby(name: String) {
+    Column(Modifier
+        .padding(50.dp)
+    ) {
+        Text(text = "By $name!")
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyMentoringTheme {
-        Greeting("Android")
+        Screen()
     }
 }
